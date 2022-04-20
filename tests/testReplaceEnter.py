5bb947c7eef928c1172549bd31e4a01f8a299fef
t-yama11 <am80sltz@icloud.com>
2022-04-21 00:04:52 +0900

    引数が適切で無い場合のテストを行った.

--- tests/testReplaceEnter.py
+++ tests/testReplaceEnter.py
@@ -1,15 +1,23 @@
 import sys
-import pytest
 
 sys.path.append("../")
 from main import replace_enter
 
 
 # < ------------- 以下がテストコード -------------- >
-def test_correct_arguments():
-    tar_str = 'Hello.\n My name is Taro.\n'
+def test_is_correct_arguments():
+    tar_str = 'Hello.\nMy name is Taro.\n'
     char_before_rep = '\n'
     char_after_rep = ' '
 
-    expect_str = "Hello. My name is Taro. "
+    expect_str = 'Hello. My name is Taro. '
+    assert replace_enter(tar_str, char_before_rep, char_after_rep) == expect_str
+
+
+def test_is_not_correct_arguments():
+    tar_str = 'Hello.\nMy name is Taro.\n'
+    char_before_rep = '\n'
+    char_after_rep = ' '
+
+    expect_str = ''
     assert replace_enter(tar_str, char_before_rep, char_after_rep) == expect_str
