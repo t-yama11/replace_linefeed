d897fd8ab1291b9973aeaa96f8962708112b946f
t-yama11 <am80sltz@icloud.com>
2022-04-21 19:39:57 +0900

    テストコードを修正した.

--- tests/testReplaceEnter.py
+++ tests/testReplaceEnter.py
@@ -17,7 +17,7 @@ def test_is_correct_arguments():
 def test_is_not_correct_arguments():
     tar_str = 'Hello.\nMy name is Taro.\n'
     char_before_rep = '\n'
-    char_after_rep = ' '
+    char_after_rep = 1
 
     expect_str = ''
     assert replace_enter(tar_str, char_before_rep, char_after_rep) == expect_str
